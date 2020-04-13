9810eec0c42e03ad2431d6fe56cb4c5fa187f4ab
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-13 20:38:30 +0200

    Install mongoose and create schema

--- tokens/tokens.router.ts
+++ tokens/tokens.router.ts
@@ -5,8 +5,20 @@ import { Token } from './tokens.model'
 class TokensRouter extends Router {
   applyRoutes(application: restify.Server) {
     application.get('/tokens', (req, resp, next) => {
-      Token.findAll().then(tokens => {
+      Token.find().then(tokens => {
         resp.json(tokens)
+        return next()
+      })
+    })
+
+    application.get('/tokens/:id', (req, resp, next) => {
+      Token.findById(req.params.id).then(token => {
+        if (token) {
+          resp.json(token)
+          return next()
+        }
+
+        resp.send(404)
         return next()
       })
     })
