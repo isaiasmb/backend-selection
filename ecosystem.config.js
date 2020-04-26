c3d3e3caea6b08532775a30c75ca42263a820548
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-26 14:11:31 +0200

    Change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps : [{
-    name   : "bandyer-api",
-    script : "./dist/main.js",
+    name   : 'bandyer-api',
+    script : './dist/main.js',
     instances: 0,
-    exec_mode: "cluster",
+    exec_mode: 'cluster',
     watch: true,
     merge_logs: true,
     env: {
       SERVER_PORT: 5000,
       DB_URL: 'mongodb://localhost/bandyer',
-      NODE_ENV: "development"
+      NODE_ENV: 'development'
     }
   }]
 }
