223cdcfff3f8f8b8d968b4d74b85a8f6d1588712
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-26 18:34:45 +0200

    Add PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,13 +7,9 @@ module.exports = {
     watch: true,
     merge_logs: true,
     env: {
-      SERVER_PORT: 5000,
+      PORT: 5000,
       DB_URL: 'mongodb://localhost/bandyer',
       NODE_ENV: 'development'
-    },
-    env_production: {
-      SERVER_PORT: 5001,
-      NODE_ENV: "production"
     }
   }]
 }
