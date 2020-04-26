af55456b8b6c464451b4974a469e87ad61575fe3
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-26 18:28:11 +0200

    Change port

--- common/environment.ts
+++ common/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  server: { port: process.env.SERVER_PORT || 3000 },
+  server: { port: process.env.PORT || 3000 },
   db: { url: process.env.DB_URL || 'mongodb://localhost/bandyer' }
 }
